d5704345545bb16361e5451a892860bb1bb3b2d3
angel wang <bweng3310@gmail.com>
2021-11-04 14:52:03 +0800

    first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Complete from './pages/Complete'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Checkout from './pages/Checkout'
+import Notfound from './pages/Notfound'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
+// import Checkout2 from './pages/Checkout2'
+
+import Checkout2 from './pages/Checkout2'
+
+
+const App = () => {
+
+    const [paytype, setpaytype] = useState("")
+    const [choosepayment, setchoosepayment] =useState('')
+
+
+    async function Data(){
+        try
+        {
+          const data = await axios({
+            method: 'get',
+            url: "http://localhost:4000/HELLO/list",
+            headers: {
+              'Content-Type': 'application/json', 
+            }
+          })
+          const set = setpaytype(data)
+          
+          
+          console.log(set)
+        }
+        catch (error) 
+        {
+            alert("GET Error!!");    
+        } 
+       }
+
+        useEffect(() => {
+            Data()
+        },[])
+            
+    
+
+return(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" render={() =><Checkout2 paytype={paytype} choosepayment={choosepayment} setchoosepayment={setchoosepayment} />} exact />
+            <Route path="/checkout">
+                { choosepayment !== "" ? <Checkout/> :<Redirect to="/"/>}
+            </Route>
+            <Route path="/complete" component={Complete}/>
+            <Route component={Notfound}/>
+        </Switch>
+    </BrowserRouter>
+    )
+   
 }
 
-export default App;
+export default App
